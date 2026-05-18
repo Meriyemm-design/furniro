6de872d0997c380ebbe02c37eeec3b118abd2e75
Maryam-Khan-21 <meriemk0731@gmail.com>
2026-05-18 14:48:48 -0700

    minor fixes

--- src/components/FtBanner.tsx
+++ src/components/FtBanner.tsx
@@ -1,35 +1,43 @@
 import React from 'react'
 import '@styles/ftbanner.scss'
-import { FaShippingFast, FaTrophy, FaHeadset } from 'react-icons/fa'
-import { BsShieldCheck } from 'react-icons/bs'
+// import { FaShippingFast, FaTrophy, FaHeadset } from 'react-icons/fa'
+// import { BsShieldCheck } from 'react-icons/bs'
 
+import trophy from '@assets/trophy.webp';
+import guarantee from '@assets/guarantee.webp';
+import shipping from '@assets/shipping.webp';
+import customer_support from '@assets/customer_support.webp';
 
 const features = [
     {
-      id: 1,
-      icon: <FaTrophy />,
-      title: "High Quality",
-      subtitle: "crafted from top materials",
+        id: 1,
+        //   icon: <FaTrophy />,
+        img: trophy,
+        title: "High Quality",
+        subtitle: "crafted from top materials",
     },
     {
-      id: 2,
-      icon: <BsShieldCheck />,
-      title: "Warranty Protection",
-      subtitle: "Over 2 years",
+        id: 2,
+        // icon: <BsShieldCheck />,
+        img: guarantee,
+        title: "Warranty Protection",
+        subtitle: "Over 2 years",
     },
     {
-      id: 3,
-      icon: <FaShippingFast />,
-      title: "Free Shipping",
-      subtitle: "Order over 150 $",
+        id: 3,
+        // icon: <FaShippingFast />,
+        img: shipping,
+        title: "Free Shipping",
+        subtitle: "Order over 150 $",
     },
     {
-      id: 4,
-      icon: <FaHeadset />,
-      title: "24 / 7 Support",
-      subtitle: "Dedicated support",
+        id: 4,
+        // icon: <FaHeadset />,
+        img: customer_support,
+        title: "24 / 7 Support",
+        subtitle: "Dedicated support",
     },
-  ];
+];
 
 const FtBanner = () => {
     return (
@@ -39,7 +47,9 @@ const FtBanner = () => {
                     <div className="feature-grid">
                         {features.map((feature) => (
                             <div className="feature-card" key={feature.id}>
-                                <div className="feature-icon">{feature.icon}</div>
+                                <div className="feature-icon">
+                                    <img src={feature.img} alt={feature.title} />
+                                </div>
                                 <div className="feature-text">
                                     <h3>{feature.title}</h3>
                                     <p>{feature.subtitle}</p>
